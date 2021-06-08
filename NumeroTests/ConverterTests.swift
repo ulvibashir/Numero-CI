ad25141138985ff4dd003b0cd40c0df2168a220f
Ulvi Bashirov <ulvi.bashirov@ibar.az>
2021-06-08 16:33:32 +0400

    test

--- NumeroTests/ConverterTests.swift
+++ NumeroTests/ConverterTests.swift
@@ -49,9 +49,6 @@ class ConverterTests: XCTestCase {
     
     func testConversionForTwo() {
       let result = converter.convert(2)
-        
-        
-        
       XCTAssertEqual(result, "II", "Conversion for 2 is incorrect")
     }
 
